ca9c3c36a2e3f12ead6330f3a2a111a795caa9f0
CasperWychgel <casper.wychgel.github@gmail.com>
2019-10-06 18:14:16 +0200

    webapp template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,105 +3,138 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
+        <!--Fonts-->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize/materialize.css"  media="screen,projection"/>
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Coolcast</title>
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+    <section id="navigation">
+        <nav>
+            <div class="nav-wrapper white">
+                <a href="index.php" class="brand-logo grey-text darken-2 center"><span style="color: #94c7ff">CoolCast</span></a>
+                <a data-target="slide-out" style="display: block" class="sidenav-trigger"><i style="color: black" class="material-icons ">menu</i></a>
             </div>
-        </div>
+        </nav>
+
+        <ul id="slide-out" class="sidenav" style="transform: translateX(-105%);">
+            <li><div class="user-view">
+                    <div class="background" style="background-color: #94c7ff"></div>
+                    <a><img class="circle" src="img/logo/Fridge.svg"></a>
+                    <a><span class="white-text name">CoolCast</span></a>
+                    <a href=""><span class="white-text email">info@coolcast.nl</span></a>
+                </div></li>
+            <li><a class="subheader">Your locations</a></li>
+            <li><a href="">Keuken</a></li>
+            <li><a href="">Voorraadkast</a></li>
+            <li><a href="">Bijkeuken</a></li>
+
+
+        </ul>
+    </section>
+
+    <section id="foodcollection">
+        <ul class="collection">
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+            <li class="collection-item avatar">
+                <i class="material-icons circle light-blue">fastfood</i>
+                <span class="title">Food example</span>
+                <p>Lorem ipsum <br>
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                </p>
+                <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+            </li>
+
+        </ul>
+    </section>
+
+
+    <script type="text/javascript" src="js/materialize/materialize.min.js"></script>
+    <script type="text/javascript" src="js/init.js"></script>
     </body>
-    <!-- Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
