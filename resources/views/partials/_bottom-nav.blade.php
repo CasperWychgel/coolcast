97bed2e663b3a2edaee8fc78a1992a73e77786c0
Sunny de Graaf <sunnydegraaf@hotmail.nl>
2019-11-19 20:24:00 +0100

    Added search option to add product input field

---
+++ resources/views/partials/_bottom-nav.blade.php
@@ -0,0 +1,11 @@
+<div class="d-flex">
+    <div class="m-4 p-1 bg-primary rounded-circle">
+        <i class="fas fa-plus"></i>
+    </div>
+    <div class="m-4 p-1 bg-primary rounded-circle">
+        <i class="fas fa-times"></i>
+    </div>
+    <div class="m-4 p-1 bg-primary rounded-circle">
+        <i class="far fa-trash-alt"></i>
+    </div>
+</div>
