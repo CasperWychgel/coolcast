8d8f454a1ee6ca599220d75ba9d4ca08c7deecff
Sunny de Graaf <sunnydegraaf@hotmail.nl>
2019-11-21 16:28:18 +0100

    Disabled toolbar and edited location inputfield

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -6,7 +6,7 @@
 
         <p>What is the location called?</p>
 
-        <input name="name" list="brow" placeholder="Product">
+        <input name="name" list="brow" placeholder="Location">
 
         <div class="">
             <button class="" type="submit" name="action">Submit</button>
