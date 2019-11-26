afae7fe7f891851c242c61c787eb785e4bb797dd
TAThiemo <teetjemo@gmail.com>
2019-11-26 14:53:50 +0100

    sorting by date
    added functionality to sort  by date for the products inside the fridge.

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,8 +5,9 @@
 
     <div class="">
         @foreach ($invproducts as $invproduct)
-            <a class="">{{$invproduct->name}}</a>
+            <a class="">{{$invproduct->name}}</a> <a href="">{{$invproduct->date}}</a>
             <br>
+
         @endforeach
     </div>
 
