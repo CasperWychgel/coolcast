30989ce1306b93eded235724403b6e16554bbcbe
TAThiemo <teetjemo@gmail.com>
2019-12-03 15:40:07 +0100

    Nav bottom
    Nav bottom

--- resources/views/partials/_nav-bottom.blade.php
+++ resources/views/partials/_nav-bottom.blade.php
@@ -1,12 +1,6 @@
-<ul class="nav justify-content-center fixed-bottom">
-    <li class="nav-item">
-        <a class="nav-link" href="/locations/add">Add a Location</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="/add">Add a product</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Work in progress</a>
-    </li>
-</ul>
 
+            <div class="d-flex justify-content-center fixed-bottom mb-5">
+                <a href="" class="btn-circle btn-danger mr-2 align-self-center d-flex"><img src="../img/delete.png" alt="" width="20" height="20" class="align-self-center ml-3"></a>
+                <a href="" class="btn-circle-l btn-info align-self-center d-flex"><img src="../img/add.png" alt="" width="20" height="20" class="align-self-center center ml-4"></a>
+                <a href="" class="btn-circle btn-danger ml-2 align-self-center d-flex"><img src="../img/edit.png" alt="" width="20" height="20" class="align-self-center center ml-3"></a>
+            </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,7 @@
             <p class="lead text-center">Hieronder zie je een overzicht van alle producten in je huis.</p>
         </div>
     </div>
-    <div class="container">
+    {{-- <div class="container">
         <table class="table table-hover table-borderless mt-0">
             <thead>
             <tr>
@@ -35,11 +35,8 @@
                 <div class="progress-bar bg-info" role="progressbar" style="width: 68%" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100">0,856 Ton CO2</div>
             </div>
         </div>
-    </div>
+    </div> --}}
 
-        <div class="card-header">
-            Your products
-        </div>
             <div class="card-body">
                 @foreach ($invproducts as $invproduct)
                 <div class="card mb-2" style="">
@@ -51,12 +48,4 @@
                 @endforeach
             </div>
 
-            <div class="d-flex justify-content-center pt-5">
-                    <button type="button" class="btn-primary btn-circle btn-xl mr-2 bg-blue"><img src="../img/delete.png" alt="" width="20" height="20">
-                    </button>
-                    <button type="button" class="btn-success btn-circle-l bg-darkblue"><img src="../img/add.png" alt="" width="20" height="20">
-                    </button>
-                    <button type="button" class="btn-success btn-circle btn-xl ml-2 bg-blue"><img src="../img/edit.png" alt="" width="20" height="20">
-                    </button>
-        </div>
 @endsection
