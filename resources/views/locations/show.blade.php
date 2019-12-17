45ec7f9d6f7183f626be7f73ab7cb979dd1325f3
CasperWychgel <32338437+CasperWychgel@users.noreply.github.com>
2019-12-17 16:35:23 +0100

    Update show.blade.php

--- resources/views/locations/show.blade.php
+++ resources/views/locations/show.blade.php
@@ -9,8 +9,8 @@
                 <p class="lead"></p>
                 @endforeach
             </div>
-            <div class="container-fluid editshow">
-                <div class="custom-control custom-checkbox">
+            <div class="container-fluid">
+                <div class="custom-control custom-checkbox editshow">
                     <input type="checkbox" class="custom-control-input selectall" name="selectall" id="selectall" >
                     <label class="custom-control-label" for="selectall">Selecteer alle producten</label>
                 </div>
@@ -19,17 +19,22 @@
         <form method="post" id="deleteform">
             {{ csrf_field() }}
             <input type="hidden" name="_method" value="delete">
-            <div class="">
+            <div class="card-body">
                 @foreach ($invproducts as $invproduct)
-                    <div class="container mb-2 bg-card">
+                    <div class="container-fluid mb-2 bg-card">
                         <div class="row">
-                            <div class="col-6 ">
-                                <h5 class="card-title">{{$invproduct->name}}</h5>
-                                <p class="card-text">{{$invproduct->expiration_date}}</p>                            </div>
+                            <div class="card-body">
+                                <div class="col-6 ">
+                                    <h5 class="card-title">{{$invproduct->name}}</h5>
+                                    <p class="card-text">{{$invproduct->expiration_date}}</p>
+                                </div>
+                            </div>
                             <div class="col-6">
-                                <input type="checkbox" class="custom-control-input selectbox" name="product[]" value="{{$invproduct->id}}" id="{{$invproduct->id}}">
-                                <label class="custom-control-label" for="{{$invproduct->id}}"></label>
-                                <a class="text-white btn btn-light bg-transparent" href="/products/{{$invproduct->id}}/edit" role="button">Edit</a>
+                                <div class="editshow">
+                                    <input type="checkbox" class="custom-control-input selectbox" name="product[]" value="{{$invproduct->id}}" id="{{$invproduct->id}}">
+                                    <label class="custom-control-label" for="{{$invproduct->id}}"></label>
+                                    <a class="text-white btn btn-light bg-transparent" href="/products/{{$invproduct->id}}/edit" role="button">Edit</a>
+                                </div>
                             </div>
                         </div>
                     </div>
