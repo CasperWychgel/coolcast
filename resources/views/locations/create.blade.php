8ba0299ad052c954061dec86a7d73b2a4fee33ca
TAThiemo <teetjemo@gmail.com>
2019-12-19 09:27:47 +0100

    Pages fix
    snel gedaan hoop dat t werkt

--- resources/views/locations/create.blade.php
+++ resources/views/locations/create.blade.php
@@ -3,16 +3,24 @@
 @section('content')
 
     @include('partials._error')
-
-    <form id="" method="POST" action="/locations/add" >
-        {{csrf_field()}}
-
-        <p>What is the location called?</p>
-
-        <input name="name" list="brow" placeholder="Location">
-
-        <div class="">
-            <button class="" type="submit" name="action">Submit</button>
+    <div class="jumbotron jumbotron-fluid text-black bg-transparent mb-0 bg">
+        <div class="container">
+            <h1 class="display-4 text-center">Nieuwe locaties toevoegen</h1>
+            <p class="lead text-center">Hier is het mogelijk om een nieuwe locatie toe te voegen aan je CoolCast</p>
         </div>
-    </form>
+    </div>
+
+    <div class="container text-black">
+        <form id="" method="POST" action="/locations/add" >
+            {{csrf_field()}}
+            <p class="lead text-center">What is the location called?</p>
+            <div class="input-group mb-3 shadow-lg">
+                <div class="input-group-prepend">
+                    <label class="input-group-text" for="inputGroupSelect02">Locatie</label>
+                </div>
+                <input name="location" class="form-control">
+            </div>
+            <button type="submit" name="action" class="text-white btn btn-block btn-info bg-darkblue shadow-lg">Add to your locations</button>
+        </form>
+    </div>
 @endsection
